7b559b32da47e4a91a72bd99bd11bf60be39fdf5
Hector OG <59241967+CI5KO@users.noreply.github.com>
2023-12-10 11:30:12 -0600

    Defer prop

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { GamesComponent } from '../games/games.component'
+import { CommentsComponent } from '../comments/comments.component'
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [GamesComponent],
+  imports: [GamesComponent, CommentsComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
